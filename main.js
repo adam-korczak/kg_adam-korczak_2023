1f78a17a009d1e19728d1d0cee346c38cf771b5d
AdamKorczak <66699135+adam-korczak@users.noreply.github.com>
2021-05-03 15:45:20 -0400

    Update main.js
    opted to use switch case to avoid bad inputs halting the program

--- main.js
+++ main.js
@@ -1,5 +1,51 @@
-var inputArr = process.argv.slice()
+var input = process.argv.slice(2);
+  let string = '';
 
-var stringArray = ["Zero","One","Two","Three","Four","Five","Six","Seven","Eight","Nine"];
+  for (let i = 0; i < input.length; i++) {
+    let word = input[i];
 
-//console.log();
+    for (let j = 0; j < word.length; j++) {
+      let c = word[j];
+
+      switch (c) {
+        case '1':
+          string += 'One';
+          break;
+        case '2':
+          string += 'Two';
+          break;
+        case '3':
+          string += 'Three';
+          break;
+        case '4':
+          string += 'Four';
+          break;
+        case '5':
+          string += 'Five';
+          break;
+        case '6':
+          string += 'Six';
+          break;
+        case '7':
+          string += 'Seven';
+          break;
+        case '8':
+          string += 'Eight';
+          break;
+        case '9':
+          string += 'Nine';
+          break;
+        case '0':
+          string += 'Zero';
+          break;
+        default:
+          break;
+      }
+
+      if (j >= word.length - 1 && i != input.length - 1) {
+        string += ', ';
+      }
+    }
+  }
+
+console.log(string);
